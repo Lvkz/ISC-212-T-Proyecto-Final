f22fee4a67d2df050cbec50749633f902bf989a3
Cesardarinel <cesardarinel@hotmail.com>
2013-04-19 17:55:25 -0400

    Servidor
    para conectar al servidor tienes que ponerle la ip de la pc

--- PortafolioFinal_Chat/PortafolioFinal_Chat/Activity1.cs
+++ PortafolioFinal_Chat/PortafolioFinal_Chat/Activity1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Text;
+using System.Net.Sockets;
+
 
 using Android.App;
 using Android.Content;
@@ -17,16 +20,38 @@ namespace PortafolioFinal_Chat
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			TcpClient Cliente;
+			NetworkStream StreamCliente;
+			string mensaje;
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+			//Button button = FindViewById<Button> (Resource.Id.myButton);
+			Button Boton_login= FindViewById<Button> (Resource.Id.button2);
+
+			Boton_login.Click += delegate {
+				try
+				{
+					Cliente = new TcpClient("10.0.0.2", 8888);
+					StreamCliente = Cliente.GetStream();
+					byte[] data = Encoding.ASCII.GetBytes("Cesar");
+					StreamCliente.Write(data, 0, data.Length);
+					StreamCliente.Flush();
+					mensaje = "Conectando Con el Servidor";
+					//Mensaje_REcivido();
+					//Thread Hilo = new Thread(Recivir_Mensaje);
+					//Hilo.Start();
+					
+				}
+				catch 
+				{
+					mensaje=("Error Al conectar");
+				}
+				Boton_login.Text=mensaje;
+
 			};
 		}
 	}
--- PortafolioFinal_Chat/PortafolioFinal_Chat/obj/Debug/android/src/portafoliofinal_chat/portafoliofinal_chat/R.java
+++ PortafolioFinal_Chat/PortafolioFinal_Chat/obj/Debug/android/src/portafoliofinal_chat/portafoliofinal_chat/R.java
@@ -15,12 +15,11 @@ public final class R {
         public static final int monoandroidsplash=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f060004;
-        public static final int button2=0x7f060005;
-        public static final int linearLayout1=0x7f060003;
-        public static final int myButton=0x7f060000;
-        public static final int textboxContraseña=0x7f060002;
-        public static final int textboxNombre=0x7f060001;
+        public static final int button1=0x7f060003;
+        public static final int button2=0x7f060004;
+        public static final int linearLayout1=0x7f060002;
+        public static final int textboxContraseña=0x7f060001;
+        public static final int textboxNombre=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
